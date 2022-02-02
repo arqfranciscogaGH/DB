26e947093c08d8d52c8aaf99e3f01ac112d2eb99
Francisco Garcia <52688111+arqfranciscogaGH@users.noreply.github.com>
2022-02-02 01:34:31 -0600

    Migracion   Inicial
    Migracion   Inicial

---
+++ Scripts/drp.sql
@@ -0,0 +1,90 @@
+use DB_A72B95_drp
+
+select *from    ModuloSistema
+select *from   SesionUsuario
+select *from   SesionUsuarioHistorial
+
+select *from    CuentaUsuario
+select * from   Persona
+select * from Privilegio
+select * from PrivilegioAsignado
+select * from Aplicacion
+select * from Configuracion
+
+TipoValidacion
+TipoControl
+ElementoCatalogoIdioma
+ElementoCatalogo
+Catalogo
+OpcionMenuIdioma
+OpcionMenu
+Menu
+
+AccesoCaptura
+DefinicionCaptura
+PlaneacionCaptura
+ContenedorControl
+
+PropiedadControlIdioma
+PropiedadControl
+AccionControl
+
+Perfil
+Grupo
+UnidadNegocio
+CentroCosto
+Area
+Domicilio
+Empresa
+Empleado
+Persona
+
+Captura
+Idioma
+ClasificacionCaptura
+RespuestaCaptura
+RespuestaPregunta
+
+Suscripcion
+Puesto
+Terminologia
+TerminologiaIdioma
+
+ErrorPersonalizado
+BitacoraError
+
+
+Pregunta
+PreguntaRespuesta
+Pais
+EstatusSolictud
+
+Sistema
+TipoSolicitud
+Solicitud
+Proyecto
+ProyectoUsuario
+TipoActividad
+ActividadProyecto
+
+ESTADO
+MUNICIPIO
+LOCALIDAD
+AGEBS
+DENUE
+
+select count(*) from ESTADO
+select count(*) from MUNICIPIO
+select count(*) from LOCALIDAD
+select count(*) from AGEBS
+select count(*) from DENUE
+
+select * from 
+
+ObtenerPrivilegios
+ObtenerCuestionarios
+
+ObtenerControles
+
+sp_help CuentaUsuario
+
---
+++ Scripts/drp2.sql
@@ -0,0 +1,24 @@
+
+use DB_A72B95_drp2
+
+
+select *from    ModuloSistema
+select *from   SesionUsuario
+select *from   SesionUsuarioHistorial
+select *from    CuentaUsuario
+select * from   Persona
+select * from Privilegio
+select * from PrivilegioAsignado
+select * from Aplicacion
+select * from Configuracion
+
+select * from 
+ObtenerPrivilegios
+
+sp_help CuentaUsuario
+
+select count(*) from ESTADO
+select count(*) from MUNICIPIO
+select count(*) from LOCALIDAD
+select count(*) from AGEBS
+select count(*) from DENUE
